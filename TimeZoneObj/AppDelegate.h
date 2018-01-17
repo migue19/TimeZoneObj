d76d8a52a997045a98d3af50c70fac76a3e83f2d
Miguel Mexicano Herrera <miguel.mexicano@gonet.us>
2018-01-17 17:06:00 -0600

    Initial Commit

---
+++ TimeZoneObj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeZoneObj
+//
+//  Created by Miguel Mexicano on 17/01/18.
+//  Copyright © 2018 Miguel Mexicano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimeZoneObj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimeZoneObj
+//
+//  Created by Miguel Mexicano on 17/01/18.
+//  Copyright © 2018 Miguel Mexicano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
