a488c4c044793314e613f142c5239c27caeb54ad
Miguel Mexicano Herrera <miguel.mexicano@gonet.us>
2018-01-19 11:14:15 -0600

    primer commit

--- TimeZoneObj/ViewController.m
+++ TimeZoneObj/ViewController.m
@@ -14,16 +14,53 @@
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSDate *fecha = [[NSDate alloc] init];
+    
+    
+    
+    NSString *MexicoDF = @"America/Mexico_City";
+    NSString *AsiaBahrain = @"Asia/Bahrain";
+    NSString *Chihuahua = @"America/Chihuahua";
+    NSString *Cancun = @"America/Cancun";
+    
+    
+    
+    NSLog(@"Hora %@: %@",MexicoDF,[self changeHourTimeZone:fecha TimeZone: MexicoDF]);//GMT-6
+    NSLog(@"Hora %@: %@",AsiaBahrain,[self changeHourTimeZone:fecha TimeZone: AsiaBahrain]);//GMT+3
+    NSLog(@"Hora %@: %@",Chihuahua,[self changeHourTimeZone:fecha TimeZone: Chihuahua]);//GMT-7
+    NSLog(@"Hora %@: %@",Cancun,[self changeHourTimeZone:fecha TimeZone: Cancun]); //GMT-5
+
 }
+
+
+
+-(NSString *)changeHourTimeZone: (NSDate *) date TimeZone: (NSString *) strTimeZone{
+    
+    NSTimeZone *timeZone = [[NSTimeZone alloc] initWithName: strTimeZone];
+    
+    NSDateFormatter *formatt = [[NSDateFormatter alloc] init];
+    [formatt setTimeZone: timeZone];
+    [formatt setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSString *strDate = [formatt stringFromDate:date];
+    
+    return strDate;
+}
+
+
+
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
 
 
 @end
